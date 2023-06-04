2d38b7891d004eedad558f9cb266b9fa2d56fe12
ghassan-sys <77061886+ghassan-sys@users.noreply.github.com>
2023-06-04 13:06:51 +0300

    Update templateAcc.scala

--- src/main/scala/templateAcc.scala
+++ src/main/scala/templateAcc.scala
@@ -4,9 +4,10 @@
 package templateAcc
 
 import Chisel._
+import chisel3.experimental.{IntParam, BaseModule}
 import chisel3.util.{HasBlackBoxResource}
 import freechips.rocketchip.tile._
-import org.chipsalliance.cde.config._
+import freechips.rocketchip.config._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.rocket.{TLBConfig, HellaCacheReq}
 
@@ -16,21 +17,21 @@ class WrapBundle(nPTWPorts: Int)(implicit p: Parameters) extends Bundle {
   val reset = Input(UInt(1.W))
 }
 
-class templateAccBlackBox(implicit p: Parameters) extends 
-	BlackBox(Map("DATA_WIDTH" -> IntParam(p.DATA_WIDTH), "ADDR_WIDTH" -> IntParam(p.ADDR_WIDTH), "CFG_REG_WIDTH" -> IntParam(p.CFG_REG_WIDTH), "NUM_OF_CFG_REGS" -> IntParam(p.NUM_OF_CFG_REGS), "MEM_DATA_WIDTH" -> IntParam(p.MEM_DATA_WIDTH), "BUFF_SIZE" -> IntParam(p.BUFF_SIZE), "LATENCY" -> IntParam(p.LATENCY))) 
+class templateAccBlackBox(tap: TemplateAccParams)(implicit p: Parameters) extends 
+	BlackBox(Map("DATA_WIDTH" -> IntParam(tap.DATA_WIDTH), "ADDR_WIDTH" -> IntParam(tap.ADDR_WIDTH), "CFG_REG_WIDTH" -> IntParam(tap.CFG_REG_WIDTH), "NUM_OF_CFG_REGS" -> IntParam(tap.NUM_OF_CFG_REGS), "MEM_DATA_WIDTH" -> IntParam(tap.MEM_DATA_WIDTH), "BUFF_SIZE" -> IntParam(tap.BUFF_SIZE), "LATENCY" -> IntParam(tap.LATENCY))) 
 		with HasBlackBoxResource {
   val io = IO(new WrapBundle(0))
 
-  addResource("/vsrc/accTemplate.v")
+  addResource("/vsrc/templateAccBlackBox.v")
 }
 
-class templateAcc(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(opcodes = opcodes) {
+class templateAcc(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(opcodes = opcodes, nPTWPorts = 0) {
   override lazy val module = new templateAccImp(this)
 }
 
 class templateAccImp(outer: templateAcc)(implicit p: Parameters) extends LazyRoCCModuleImp(outer) {
 
-    def params : TemplateAccParams
+    def params : TemplateAccParams ={TemplateAccParams()}
     val templateaccbb = Module(new templateAccBlackBox(params))
     io <> templateaccbb.io.io
     templateaccbb.io.clock := clock
@@ -44,7 +45,7 @@ case class TemplateAccParams(
   NUM_OF_CFG_REGS: Int = 3,
   MEM_DATA_WIDTH: Int = 8,
   BUFF_SIZE: Int = 32,
-  LATANCY: Int = 500,
+  LATENCY: Int = 500,
   MEMORY_BANDWIDTH: Int = 300)
   
 
@@ -56,5 +57,4 @@ class WithTemplateAccBlackBox extends Config((site, here, up) => {
     }
 )	 
 })
-
 
